20ec6685f6aef0b742e6624a7fca22653e1a3ad5
saugat694260 <khadkasaugat321@gmail.com>
2024-01-24 13:02:57 +0545

    fixed..beta review section omplete

--- scripts/selectedProductPage.js
+++ scripts/selectedProductPage.js
@@ -21,9 +21,11 @@ export function savetostorage() {
 
 
 //gives id of product to select reviews of specific id
-selectedProductArray.forEach((data)=>{
-  selectReviewFromDatabase(data.id);
-})
+if(maincontainer){
+  selectedProductArray.forEach((data)=>{
+    selectReviewFromDatabase(data.id);
+  })
+}
 
 //this function takes the id and adds the data for the selected container only
 //takes the data pushes in to in to array and becomes empty again so it can only give one array to new array if itself is not empty ..the new array is stored in local storage so it wont dissapear
